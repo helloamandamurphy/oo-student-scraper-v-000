a2d29886a06309653b00beeb08a406e922a75771
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-23 20:28:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -12,15 +12,14 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    self.new(Scraper.scrape_index_page)
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
